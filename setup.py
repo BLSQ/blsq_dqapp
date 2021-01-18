893d5f4f25f3b29e20ce058cb6d96adb7dc74fe1
FVB-BLSQ <fvaldesbango@bluesquarehub.com>
2021-01-18 17:44:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-with open('LICENSE') as f:
+with open('LICENSE.txt') as f:
     license = f.read()
     
 setup(
