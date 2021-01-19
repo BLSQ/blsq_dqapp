c59b6a75033ebf93eb7523d0d8e0e76ac768a790
FVB-BLSQ <fvaldesbango@bluesquarehub.com>
2021-01-19 11:00:59 +0100

    Update outlier_detection.py

--- blsq_dqapp/outlier_detection.py
+++ blsq_dqapp/outlier_detection.py
@@ -54,7 +54,7 @@ def df_median_batching_assignment(df,re=None):
     """""
     
     #To reduce computations we only proceed to calculate if any data is present.
-    if not np.isnan(df.VALUE.max()):
+    if not np.isnan(df['VALUE'].max()):
         
         median_units_values=median_unit_values_reviewed_generation(df,re)
         
