beb5f6a46b28c78b3595c46034e9cdfb2a299c33
FVB-BLSQ <fvaldesbango@bluesquarehub.com>
2021-01-20 10:47:47 +0100

    Add de metadata to quality container

--- blsq_dqapp/quality_container.py
+++ blsq_dqapp/quality_container.py
@@ -19,7 +19,7 @@ class quality_auction_container(object):
 
     """
 
-    def __init__(self, input_value_df,input_metadata_tree,project_path_processed,files_main_name,de_uid_vars):
+    def __init__(self, input_value_df,input_metadata_tree,input_metadata_de,project_path_processed,files_main_name,de_uid_vars):
         """Create a dhis instance."""
         self.processed_df = input_value_df
         self.input_metadata_tree = input_metadata_tree
@@ -27,6 +27,7 @@ class quality_auction_container(object):
         self.files_main_name = files_main_name
         self.de_uid_vars=de_uid_vars
         self.period_table=self.processed_df[['PERIOD']].drop_duplicates()
+        self.input_metadata_de=input_metadata_de
         
     def outliers_generation(self):
         self.processed_df=outlier_detection_handler(self.processed_df,
@@ -35,7 +36,7 @@ class quality_auction_container(object):
                                                     self.files_main_name)
             
     def availability_generation(self,custom_tree_input=None,level_to_filter=5):
-        if not custom_tree_input:
+        if custom_tree_input==None:
             fosa_tree_expected=self.input_metadata_tree.query('LEVEL=='+str(level_to_filter))
             fosa_tree_expected=fosa_tree_expected[['OU_UID']]
             custom_tree_input=cross_join(fosa_tree_expected,self.period_table)
