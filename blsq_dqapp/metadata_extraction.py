631d8d85056032c2b575ed22d09a5ab263676038
FVB-BLSQ <fvaldesbango@bluesquarehub.com>
2021-09-13 11:37:05 +0200

    changed urls queries saving pattern and modified printed text

--- blsq_dqapp/metadata_extraction.py
+++ blsq_dqapp/metadata_extraction.py
@@ -296,28 +296,37 @@ class Dhis2Client(object):
             printedText="Call processing"
         
         coc_default_uid=self.fetch_coc_structure().query('COC_NAME=="'+coc_default_name+'"').COC_UID.values[0]
+        
+        url_queries_list=[]
         for dx_batch_descriptor in dx_batchted_descriptors:
             for ou_batch_descriptor in ou_batchted_descriptors:
                 url_analytics =url_analytics_base+'?dimension='+self._dx_composer_feed(dx_batch_descriptor)
                 url_analytics =url_analytics+'&dimension='+self._ou_composer_feed(ou_batch_descriptor)
                 url_analytics =url_analytics+'&dimension='+self._pe_composer_feed(pe_start_date,pe_end_date,frequency)
+                url_queries_list.append(url_analytics)
                 
+        total_queries=len(url_queries_list)
+        batch_index=1
         
-                print(printedText)
+        for url_query in total_queries:
+        
+            print(printedText, f' : batch_index/{total_queries}')
             
-                resp_analytics = self.session.get(url_analytics)
-                if not silent:
-                    print(resp_analytics.request.path_url)
+            batch_index +=1
         
-                analyticsData_batch=resp_analytics.json()['rows']
-                
-                if not analyticsData_batch:
-                    print( "No Data in DB")
-                    pass 
-                else:
-                    analyticsData_df_list.append(
-                                                self._analytics_json_to_df(analyticsData_batch,coc_default_uid=coc_default_uid)
-                                                )
+            resp_analytics = self.session.get(url_query)
+            if not silent:
+                print(resp_analytics.request.path_url)
+    
+            analyticsData_batch=resp_analytics.json()['rows']
+            
+            if not analyticsData_batch:
+                print( "No Data in DB")
+                pass 
+            else:
+                analyticsData_df_list.append(
+                                            self._analytics_json_to_df(analyticsData_batch,coc_default_uid=coc_default_uid)
+                                            )
                     
             analyticsData=pd.concat(analyticsData_df_list,ignore_index=True)
                     
